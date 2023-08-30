36bd6399b529d7d54a5a4f7737afb16868bc4f77
invdev778 <inv.dev778@gmail.com>
2023-08-30 15:53:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/aLog/ALogApplication.java
@@ -0,0 +1,13 @@
+package com.aLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ALogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aLog/ALogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ALogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
