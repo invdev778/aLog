c40e35f36a9f495b23d29084780c6c9f58af68a3
invdev778 <inv.dev778@gmail.com>
2023-09-01 21:43:23 +0900

    등록/수정/조회 API 구현

---
+++ src/main/java/com/aLog/service/PostsService.java
@@ -0,0 +1,40 @@
+package com.aLog.service;
+
+import com.aLog.domain.posts.Posts;
+import com.aLog.domain.posts.PostsRepository;
+import com.aLog.dto.PostsResponseDto;
+import com.aLog.dto.PostsSaveRequestDto;
+import com.aLog.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id"+ id));
+
+        return new PostsResponseDto(entity);
+    }
+}
