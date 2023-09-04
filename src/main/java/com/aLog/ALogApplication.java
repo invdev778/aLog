5da074f23f14408134b62677be89b168ed9297ce
invdev778 <inv.dev778@gmail.com>
2023-09-05 00:34:59 +0900

    JPA Auditing

--- src/main/java/com/aLog/ALogApplication.java
+++ src/main/java/com/aLog/ALogApplication.java
@@ -2,7 +2,9 @@ package com.aLog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class ALogApplication {
 	public static void main(String[] args) {
