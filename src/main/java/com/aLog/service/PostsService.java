e730a25ebd3e68c2c43ed8d6ebde4a2bb2621e53
invdev778 <inv.dev778@gmail.com>
2023-09-08 01:25:15 +0900

    게시글 조회 페이지 구현

--- src/main/java/com/aLog/service/PostsService.java
+++ src/main/java/com/aLog/service/PostsService.java
@@ -2,13 +2,17 @@ package com.aLog.service;
 
 import com.aLog.domain.posts.Posts;
 import com.aLog.domain.posts.PostsRepository;
+import com.aLog.dto.PostsListResponseDto;
 import com.aLog.dto.PostsResponseDto;
 import com.aLog.dto.PostsSaveRequestDto;
 import com.aLog.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +40,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id"+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
