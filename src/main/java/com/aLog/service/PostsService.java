4519cb7acec81434d6558e94e157d16a437ea3c8
invdev778 <inv.dev778@gmail.com>
2023-09-08 16:44:54 +0900

    게시글 수정, 삭제 페이지 구현 / 코드 수정

--- src/main/java/com/aLog/service/PostsService.java
+++ src/main/java/com/aLog/service/PostsService.java
@@ -35,6 +35,15 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id"+ id));
@@ -48,4 +57,6 @@ public class PostsService {
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
     }
+
+
 }
